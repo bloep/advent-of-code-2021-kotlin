b67935b34a718b3de760a5d8b69a8055ac76ef52
Marcel Kuhmann <marcel@dermarcel.eu>
2021-12-01 21:08:09 +0100

    remove inputs
    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val nums = input.map(String::toInt);
+        return nums.slice(0 until nums.size-1)
+                .mapIndexed { i, num -> nums[i + 1] > num }
+                .filter { x -> x }
+                .size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val nums = input.map(String::toInt);
+        return nums.slice(0 until nums.size-3)
+            .mapIndexed { i, num -> (num + nums[i+1] + nums[i+2]) < (nums[i+1] + nums[i+2] + nums[i+3]) }
+            .filter { x -> x }
+            .size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
